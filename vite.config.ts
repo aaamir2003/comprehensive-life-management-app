def6b4deb976c46e0601cde6368955e525ea5d36
aaamir2003 <amirhosseinahan2003@gmail.com>
2026-08-17 19:30:27 +0330

    Fix project structure and relative base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-﻿import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/comprehensive-life-management-app/',
-})
+  base: "./", // استفاده از مسیر نسبی برای جلوگیری از خطای 404 روی گیت‌هاب پیج
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
